139e8eddb58e2ebeaae910f87c7641eb7203a396
Michel Kansou <michel.kansou@outlook.fr>
2017-07-18 18:40:15 +0200

    add PageTheScroll App

---
+++ iOS Apps/PageTheScroll/PageTheScroll/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  PageTheScroll
+//
+//  Created by Michel Kansou on 18/07/2017.
+//  Copyright © 2017 Michel Kansou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var images = [UIImageView]()
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        let scrollWidth = scrollView.frame.size.width
+        
+        for x in 0...2 {
+            let image = UIImage(named: "icon\(x).png")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+        }
+        scrollView.clipsToBounds = false
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+    }
+
+}
+
